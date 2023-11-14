39601033abe90f352531a8aa6847df59d4590655
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-14 07:54:27 -0800

    add css styling to change backgroung color and location of h1

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Initial quote with vowels replaced by dashes
-  var initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
-  var puzzleOutput = createPuzzle(initialQuote);
+  let initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
+  let puzzleOutput = createPuzzle(initialQuote);
 
   // Display the initial puzzle
   document.getElementById('puzzleResult').textContent = puzzleOutput;
 });
 
 function createPuzzle(inputString) {
-  var characters = inputString.split('');
-  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let characters = inputString.split('');
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
-  for (var i = 0; i < characters.length; i++) {
+  for (let i = 0; i < characters.length; i++) {
     if (vowels.indexOf(characters[i]) !== -1) {
       characters[i] = '-';
     }
   }
 
-  var puzzleOutput = characters.join('');
+  let puzzleOutput = characters.join('');
   return puzzleOutput;
 }
 
 function revealAnswer() {
-  var initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
+  let initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
   document.getElementById('puzzleResult').textContent = initialQuote;
 }
 
 function resetPuzzle() {
-  var initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
-  var puzzleOutput = createPuzzle(initialQuote);
+  let initialQuote = "Believe you can and you're halfway there. Theodore Roosevelt";
+  let puzzleOutput = createPuzzle(initialQuote);
   document.getElementById('puzzleResult').textContent = puzzleOutput;
 }
